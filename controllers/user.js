1361a2816fe2b2e22442daf3ae1a5b49ac69dd2d
Guillaume Naval <navalguillaume@gmail.com>
2021-07-19 14:00:56 +0200

    fixes

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
+const crypto = require("crypto");
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
